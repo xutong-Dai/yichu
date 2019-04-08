8eb5482edc3b3ac734f02841cfb9312c58500cb5
xutong-Dai <945016828@qq.com>
2019-04-08 09:16:21 +0800

    yichu

---
+++ js/footer.js
@@ -0,0 +1,15 @@
+$('footer .home').tap(function(){
+  window.location.href = './home.html'
+})
+$('footer .msg').tap(function(){
+  window.location.href = './msgdontai.html'
+})
+$('footer .huati').tap(function(){
+  window.location.href = './topic.html'
+})
+$('footer .my').tap(function(){
+  window.location.href = './personal.html'
+})
+$('footer .new').tap(function(){
+  window.location.href = './new.html'
+})
